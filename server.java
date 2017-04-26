e2984d5e62ad51a1f28afb619170dc25f7b57080
MrAmirArshad <amir.arshad57@gmail.com>
2017-04-26 13:42:20 +0800

    Update server.java

--- server.java
+++ server.java
@@ -11,14 +11,14 @@ class TCPServer
 	
 		while(true)
 		{
-		Socket connectionSocket = welcomeSocket.accept();
-		BufferedReader inFromClient =
-		new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		DataOutputStream outToClient = new DataOutputStream	(connectionSocket.getOutputStream());
-		clientSentence = inFromClient.readLine();
- 		System.out.println("Received: " + clientSentence);
-		capitalizedSentence = clientSentence.toUpperCase() + '\n';
-		outToClient.writeBytes(capitalizedSentence);
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient =
+			new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream	(connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+ 			System.out.println("Received: " + clientSentence);
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			outToClient.writeBytes(capitalizedSentence);
 		}
 	}
 }
