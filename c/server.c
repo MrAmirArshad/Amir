b65999cb80e77ebb16696cc41f0f167885da765c
MrAmirArshad <amir.arshad57@gmail.com>
2017-05-03 15:18:36 +0800

    Create server.c

---
+++ c/server.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFSIZE 1024
+
+#if 0
+
+struct in_addr {
+  unsigned int s_addr; 
+};
+
+struct sockaddr_in  {
+  unsigned short int sin_family; /* Address family */
+  unsigned short int sin_port;   /* Port number */
+  struct in_addr sin_addr;	 /* IP address */
+  unsigned char sin_zero[...];   /* Pad to size of 'struct sockaddr' */
+};
+
+struct hostent {
+  char    *h_name;        /* official name of host */
+  char    **h_aliases;    /* alias list */
+  int     h_addrtype;     /* host address type */
+  int     h_length;       /* length of address */
+  char    **h_addr_list;  /* list of addresses */
+}
+#endif
+
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  int parentfd; /* parent socket */
+  int childfd; /* child socket */
+  int portno; /* port to listen on */
+  int clientlen; /* byte size of client's address */
+  struct sockaddr_in serveraddr; /* server's addr */
+  struct sockaddr_in clientaddr; /* client addr */
+  struct hostent *hostp; /* client host info */
+  char buf[BUFSIZE]; /* message buffer */
+  char *hostaddrp; /* dotted decimal host addr string */
+  int optval; /* flag value for setsockopt */
+  int n; /* message byte size */
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+  portno = atoi(argv[1]);
+
+  parentfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (parentfd < 0) 
+    error("ERROR opening socket");
+
+  optval = 1;
+  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, 
+	     (const void *)&optval , sizeof(int));
+
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+
+  serveraddr.sin_family = AF_INET;
+
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  serveraddr.sin_port = htons((unsigned short)portno);
+
+  if (bind(parentfd, (struct sockaddr *) &serveraddr, 
+	   sizeof(serveraddr)) < 0) 
+    error("ERROR on binding");
+
+  if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
+    error("ERROR on listen");
+
+  clientlen = sizeof(clientaddr);
+  while (1) {
+
+    childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (childfd < 0) 
+      error("ERROR on accept");
+    
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL)
+      error("ERROR on gethostbyaddr");
+    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL)
+      error("ERROR on inet_ntoa\n");
+    printf("server established connection with %s (%s)\n", 
+	   hostp->h_name, hostaddrp);
+    
+    bzero(buf, BUFSIZE);
+    n = read(childfd, buf, BUFSIZE);
+    if (n < 0) 
+      error("ERROR reading from socket");
+    printf("server received %d bytes: %s", n, buf);
+    
+    int i, j = 0;
+    gets(buf);
+    i = 0;
+    j = strlen(buf) - 1;
+ 
+    while (i < j) {
+    	temp = buf[i];
+      	str[i] = buf[j];
+      	str[j] = temp;
+	i++;
+      	j--;
+    }
+	  
+    n = write(childfd, buf, strlen(buf));
+    if (n < 0) 
+      error("ERROR writing to socket");
+
+    close(childfd);
+  }
+}
