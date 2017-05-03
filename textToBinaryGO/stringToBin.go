1cada7b010fceb276b3dbcf63dc37e24528afbef
MrAmirArshad <amir.arshad57@gmail.com>
2017-05-03 16:13:01 +0800

    Update stringToBin.go

--- textToBinaryGO/stringToBin.go
+++ textToBinaryGO/stringToBin.go
@@ -15,26 +15,24 @@ func binary(s string) string {
 }
 
 func main() {
-    b, err := ioutil.ReadFile("abc.txt") // just pass the file name
+    b, err := ioutil.ReadFile("string.txt") 
     if err != nil {
         fmt.Print(err)
     }
 
-    //fmt.Println(b) // print the content as 'bytes'
+    str := string(b) 
 
-    str := string(b) // convert content to a 'string'
+    fmt.Println(str)
 
-    fmt.Println(str) // print the content as a 'string'
+    str2 := binary(str) 
 
-    str2 := binary(str) // convert the string into binary number,char by char.
+    fmt.Println(str2) 
 
-    fmt.Println(str2) // print the content as a 'string'
-
-    file, err := os.Create("abc.bin") // create a bin file
+    file, err := os.Create("string.bin") 
     if err != nil {
         return
     }
     defer file.Close()
 
-    file.WriteString(str2) // write the string binary into bin file.
+    file.WriteString(str2) 
 }
